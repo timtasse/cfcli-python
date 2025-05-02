e805acb8589a75832131e00239c036d7124af8c9
timtasse <timtasse@users.noreply.github.com>
2025-05-02 12:51:27 +0200

    add the script and modified the README

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
 # cfcli-python
+
+In Python rewritten alternative to https://github.com/danielpigott/cloudflare-cli, but only for the Cloudflare DNS part and only for modifying existing zones.
+
+The Script uses the same Configfile as the original (~/.cfcli.yml), so it can be used as drop-in replacement.
+
+The Syntax is mostly the same, but not for all Parameters.
+
+## Configfile Example
+
+```yaml
+defaults:
+    token: <cloudflare-token>
+    domain: <default-cloudflare-domain>
+```
+
+## Usage
+
+General Usage for all subcommands
+```
+usage: Cloudflare DNS CLI [-h] [-d DOMAIN] [-D] {add,rm,ls,edit,zones} ...
+
+options:
+  -h, --help            show this help message and exit
+  -d DOMAIN, --domain DOMAIN
+                        Domain name
+  -D, --debug           Enable debug mode
+
+Commands:
+  {add,rm,ls,edit,zones}
+    add                 Add a DNS record
+    rm                  Remove a DNS record
+    ls                  List DNS records
+    edit                Edit a DNS record
+    zones               List DNS zones
+```
+the 'add' subcommand to create a new record
+```
+usage: Cloudflare DNS CLI add [-h] [-t TYPE] [-l TTL] [-p PRIORITY] [--proxied] [-m COMMENT] [-T TAG] record value
+
+positional arguments:
+  record                DNS Record Name
+  value                 DNS Value
+
+options:
+  -h, --help            show this help message and exit
+  -t TYPE, --type TYPE  DNS Type
+  -l TTL, --ttl TTL     TTL
+  -p PRIORITY, --priority PRIORITY
+                        Priority
+  --proxied             Record in proxy mode
+  -m COMMENT, --comment COMMENT
+                        Comment
+  -T TAG, --tag TAG     Tag, can be used multiple times
+```
+the 'rm' subcommand to delete a record
+```
+usage: Cloudflare DNS CLI rm [-h] [-t TYPE] record
+
+positional arguments:
+  record                DNS Record Name
+
+options:
+  -h, --help            show this help message and exit
+  -t TYPE, --type TYPE  DNS Type
+```
+the 'ls' subcommand to print records, supports literal match and RegEx via parameter
+```
+usage: Cloudflare DNS CLI ls [-h] [--regex] [search]
+
+positional arguments:
+  search      Search Term, substrings or RegEx (only valid with regex option)
+
+options:
+  -h, --help  show this help message and exit
+  --regex     Enable RegEx mode for search
+```
+the 'edit' subcommand to modify an existing record
+```
+usage: Cloudflare DNS CLI edit [-h] [-t TYPE] [-l TTL] [-p PRIORITY] [--proxied] [-m COMMENT] [-T TAG] record [value]
+
+positional arguments:
+  record                DNS Record Name
+  value                 DNS Value
+
+options:
+  -h, --help            show this help message and exit
+  -t TYPE, --type TYPE  DNS Type
+  -l TTL, --ttl TTL     TTL
+  -p PRIORITY, --priority PRIORITY
+                        Priority
+  --proxied             Record in proxy mode
+  -m COMMENT, --comment COMMENT
+                        Comment
+  -T TAG, --tag TAG     Tag, can be used multiple times
+```
+
+## Example
+
+an example of the list command
+```
+cfcli ls --regex 'server\d'
+
+                                    DNS Records of Domain demo.org
+┏━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ Type ┃ Name                           ┃ Value        ┃ TTL ┃ Proxied ┃ ID                               ┃
+┡━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━╇━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+│ A    │ server1.mk.demo.org            │ 194.97.61.90 │ 120 │ No      │ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa │
+│ A    │ server1.vm.demo.org            │ 194.97.14.42 │ 60  │ No      │ bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb │
+│ A    │ server2.mk.demo.org            │ 194.97.61.91 │ 120 │ No      │ cccccccccccccccccccccccccccccccc │
+│ A    │ sevrer2.vm.demo.org            │ 194.97.14.43 │ 60  │ No      │ dddddddddddddddddddddddddddddddd │
+└──────┴────────────────────────────────┴──────────────┴─────┴─────────┴──────────────────────────────────┘
+```
---
+++ cfcli.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import datetime
+import re
+
+try:
+    import yaml
+    from cloudflare import Cloudflare, NOT_GIVEN
+    from cloudflare.types.zones import Zone
+    from cloudflare.types.dns import RecordResponse
+    from rich.console import Console
+    from rich.table import Table
+except ImportError:
+    print("Please install cloudflare>=4.0, rich and PyYAML")
+    exit(1)
+
+CONFIGFILE_SAMPLE = """defaults:
+    token: __SPECIFY_TOKEN__
+    domain: __SPECIFY_DOMAIN__"""
+DEFAULT_TYPE = "A"
+DEFAULT_TTL = 3600
+
+class CFAction:
+    def __init__(self, namespace: argparse.Namespace, parser: argparse.ArgumentParser):
+        self.namespace = namespace
+        self.cf = Cloudflare(api_token=self._get_token())
+        self.parser = parser
+        self.domain = self.get_domain()
+    def _get_token(self) -> str:
+        if os.path.exists((path := os.path.expanduser("~/.cfcli.yml"))):
+            with open(path, "r") as f:
+                values = yaml.safe_load(f)
+                return values["defaults"]["token"]
+        else:
+            print("Configfile ~/.cfcli.yml not exists, please create it, sample follows")
+            print(CONFIGFILE_SAMPLE)
+            exit(1)
+    def get_domain(self) -> str:
+        with open(os.path.expanduser("~/.cfcli.yml"), "r") as f:
+            values = yaml.safe_load(f)
+            read_domain = values["defaults"].get("domain", None)
+        if self.namespace.domain:
+            return self.namespace.domain
+        if read_domain is not None:
+            return read_domain
+        print("Domain not given as Parameter or in Configfile")
+        self.parser.print_help()
+        exit(1)
+
+
+class RemoveCFAction(CFAction):
+    def __call__(self):
+        if len((zones := self.cf.zones.list(name=self.domain)).result) > 0:
+            zone = next(iter(zones))
+            if self.namespace.debug:
+                print(f"Zone found: {zone}")
+        else:
+            print(f"Zone {self.domain} not found or not accessible")
+            exit(1)
+        if len((records := self.cf.dns.records.list(zone_id=zone.id, type=self.namespace.type, name=self.namespace.record)).result) > 0:
+            record = next(iter(records))
+            result = self.cf.dns.records.delete(zone_id=zone.id, dns_record_id=record.id)
+            if self.namespace.debug:
+                print(f"Deleted record: {record}")
+            if result.id:
+                print(f"Successfully deleted DNS record {self.namespace.record}")
+                exit()
+        else:
+            print(f"No records found for {self.namespace.record} in {self.domain}")
+            exit(1)
+
+
+class ListCFAction(CFAction):
+    def __call__(self):
+        if len((zones := self.cf.zones.list(name=self.domain)).result) > 0:
+            zone = next(iter(zones))
+            if self.namespace.debug:
+                print(f"Zone found: {zone}")
+        else:
+            print(f"Zone {self.domain} not found or not accessible")
+            exit(1)
+        records_req = self.cf.dns.records.list(zone_id=zone.id)
+        if records_req:
+            records = []
+            for page in records_req.iter_pages():
+                records.extend(page.result)
+            if self.namespace.debug:
+                print(f"Found {len(records)} DNS records in {self.domain}")
+            if self.namespace.search:
+                if self.namespace.regex:
+                    try:
+                        regex = re.compile(self.namespace.search)
+                        self.print([rec for rec in records if regex.search(rec.name)], self.domain)
+                    except re.error:
+                        print("Invalid regular expression")
+                        exit(1)
+                else:
+                    self.print([rec for rec in records if self.namespace.search in rec.name], self.domain)
+            else:
+                if len(records) >= 1000:
+                    print("\033[1mOutput truncated to 1000 records, use search filter instead\033[0m")
+                self.print(records[:1000], self.domain)
+        else:
+            print(f"No records found in {self.domain}")
+    def print(self, records: list[RecordResponse], domain: str):
+        table = Table(title=f"DNS Records of Domain {domain}")
+        columns = ("Type", "Name", "Value", "TTL", "Proxied", "ID")
+        for column in columns:
+            table.add_column(column, no_wrap=True if column != "ID" else False)
+        for record in records:
+            table.add_row(record.type, record.name, record.content,
+                          str(round(record.ttl)) if record.ttl != 1.0 else "Automatic",
+                          "Yes" if record.proxied else "No", record.id)
+        console = Console()
+        try:
+            console.print(table)
+        except BrokenPipeError:
+            pass
+
+
+class AddCFAction(CFAction):
+    def __call__(self):
+        if len((zones := self.cf.zones.list(name=self.domain)).result) > 0:
+            zone = next(iter(zones))
+            if self.namespace.debug:
+                print(f"Zone found: {zone}")
+        else:
+            print(f"Zone {self.domain} not found or not accessible")
+            exit(1)
+        record = self.cf.dns.records.create(zone_id=zone.id,
+                                       type=self.namespace.type,
+                                       name=self.namespace.record,
+                                       ttl=self.namespace.ttl,
+                                       content=self.namespace.value,
+                                       tags=self.namespace.tag if self.namespace.tag else NOT_GIVEN,
+                                       comment=self.namespace.comment if self.namespace.comment else NOT_GIVEN,
+                                       priority=self.namespace.priority if self.namespace.priority else NOT_GIVEN)
+        if self.namespace.debug:
+            print(f"Create called: {record}")
+        if record.id:
+            print(f"Successfully created DNS record {self.namespace.record}")
+
+
+class ModifyCFAction(CFAction):
+    def __call__(self):
+        if len((zones := self.cf.zones.list(name=self.domain)).result) > 0:
+            zone = next(iter(zones))
+            if self.namespace.debug:
+                print(f"Zone found: {zone}")
+        else:
+            print(f"Zone {self.domain} not found or not accessible")
+            exit(1)
+        if len((records := self.cf.dns.records.list(zone_id=zone.id, type=self.namespace.type, name=self.namespace.record)).result) > 0:
+            record = next(iter(records))
+            if self.namespace.debug:
+                print(f"Record found: {record}")
+            response = self.cf.dns.records.edit(record.id,
+                                       zone_id=zone.id,
+                                       type=self.namespace.type,
+                                       name=self.namespace.record,
+                                       ttl=self.namespace.ttl if self.namespace.ttl != DEFAULT_TTL else NOT_GIVEN,
+                                       content=self.namespace.value if self.namespace.value else NOT_GIVEN,
+                                       tags=self.namespace.tag if self.namespace.tag else NOT_GIVEN,
+                                       comment=self.namespace.comment if self.namespace.comment else NOT_GIVEN,
+                                       priority=self.namespace.priority if self.namespace.priority else NOT_GIVEN)
+            if (datetime.datetime.now(tz=datetime.UTC) - response.modified_on).seconds < 2:
+                print(f"Successfully modified DNS record {self.namespace.record}")
+            if self.namespace.debug:
+                print(f"Record modify called: {response}")
+
+
+class ZonesCFAction(CFAction):
+    def __call__(self):
+        zones = self.cf.zones.list()
+        if self.namespace.debug:
+            print(zones)
+        self.print(zones)
+    def print(self, zones: list[Zone]):
+        table = Table(title="DNS Zones")
+        table.add_column("Name", no_wrap=True)
+        table.add_column("Plan", no_wrap=True)
+        table.add_column("Active", no_wrap=True)
+        table.add_column("ID")
+        for zone in zones:
+            table.add_row(zone.name, zone.plan["name"], zone.status, zone.id)
+        console = Console()
+        try:
+            console.print(table)
+        except BrokenPipeError:
+            pass
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Cloudflare DNS CLI")
+    parser.add_argument("-d", "--domain", help="Domain name")
+    parser.add_argument("-D", "--debug", help="Enable debug mode", action="store_true")
+    sub = parser.add_subparsers(dest="command", required=True, title="Commands")
+    add = sub.add_parser("add", help="Add a DNS record")
+    add.add_argument("-t", "--type", help="DNS Type", default=DEFAULT_TYPE)
+    add.add_argument("-l", "--ttl", help="TTL", default=DEFAULT_TTL, type=int)
+    add.add_argument("-p", "--priority", help="Priority")
+    add.add_argument("--proxied", help="Record in proxy mode", action="store_true", default=False)
+    add.add_argument("-m", "--comment", help="Comment")
+    add.add_argument("-T", "--tag", help="Tag, can be used multiple times", action="append")
+    add.add_argument("record", help="DNS Record Name")
+    add.add_argument("value", help="DNS Value")
+    rm = sub.add_parser("rm", help="Remove a DNS record")
+    rm.add_argument("record", help="DNS Record Name")
+    rm.add_argument("-t", "--type", help="DNS Type", default="A")
+    ls = sub.add_parser("ls", help="List DNS records")
+    ls.add_argument("search", help="Search Term, substrings or RegEx (only valid with regex option)", default="", nargs="?")
+    ls.add_argument("--regex", help="Enable RegEx mode for search", action="store_true", default=False)
+    edit = sub.add_parser("edit", help="Edit a DNS record")
+    edit.add_argument("-t", "--type", help="DNS Type", default=DEFAULT_TYPE)
+    edit.add_argument("-l", "--ttl", help="TTL", default=DEFAULT_TTL, type=int)
+    edit.add_argument("-p", "--priority", help="Priority")
+    edit.add_argument("--proxied", help="Record in proxy mode", action="store_true", default=False)
+    edit.add_argument("-m", "--comment", help="Comment")
+    edit.add_argument("-T", "--tag", help="Tag, can be used multiple times", action="append")
+    edit.add_argument("record", help="DNS Record Name")
+    edit.add_argument("value", help="DNS Value", default="", nargs="?")
+    zones = sub.add_parser("zones", help="List DNS zones")
+
+    args = parser.parse_args()
+    if args.command == "rm":
+        RemoveCFAction(args, parser)()
+    elif args.command == "ls":
+        ListCFAction(args, parser)()
+    elif args.command == "add":
+        AddCFAction(args, parser)()
+    elif args.command == "edit":
+        ModifyCFAction(args, parser)()
+    elif args.command == "zones":
+        ZonesCFAction(args, parser)()
